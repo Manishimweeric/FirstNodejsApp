fd4b89e6d3584bea78319c4f803d59e6fc7e64c9
unknown <manishimweeric54@gmail.com>
2024-08-07 11:37:20 +0200

    change

--- Routes/protected.js
+++ Routes/protected.js
@@ -7,6 +7,4 @@ const router = express.Router();
 router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json({ message: 'This is a protected route', user: req.user });
 });
-
-
 export default router;
